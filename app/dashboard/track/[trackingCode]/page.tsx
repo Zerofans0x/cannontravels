8ad85f7c00838252c3050d2e10664efc0fdf1c10
NaijaTechBro <bakisodiq@gmail.com>
2026-09-05 16:06:01 +0100

    Socket url

--- app/dashboard/track/[trackingCode]/page.tsx
+++ app/dashboard/track/[trackingCode]/page.tsx
@@ -9,7 +9,7 @@ import { Icon } from "@iconify/react";
 import { io, Socket } from "socket.io-client";
 import { api } from "@/lib/api";
 
-const SOCKET_URL = "http://localhost:5001";
+const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:5001";
 
 export default function LiveFlightTrackingPage() {
   const params = useParams();
--- app/track/[trackingCode]/page.tsx
+++ app/track/[trackingCode]/page.tsx
@@ -7,7 +7,7 @@ import { Icon } from "@iconify/react";
 import { io, Socket } from "socket.io-client";
 import { api } from "@/lib/api";
 
-const SOCKET_URL = "http://localhost:5001";
+const SOCKET_URL = process.env.SOCKET_URL || "http://localhost:5001";
 
 export default function PublicFlightTrackingPage() {
   const params = useParams();
